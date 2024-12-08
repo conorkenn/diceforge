e4c60d0034101cf8bca0be4b6476ed94bc848e91
Conor Kennedy <conor@conorkennedy.com>
2024-12-08 15:39:41 -0500

    added roll type selector

--- frontend/src/components/DiceRollerComponent.tsx
+++ frontend/src/components/DiceRollerComponent.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { rollDice } from "../api/diceApi";
 import RollHistoryComponent from "./RollHistoryComponent";
 import { RollHistory } from "../types";
-import '../App.css';
 import NumRollsSelector from "./NumRollsSelector";
+import RollTypeSelector from "./RollTypeSelector";
 
 const DiceRoller: React.FC = () => {
   const [difficulty, setDifficulty] = useState(12);
@@ -40,13 +40,12 @@ const DiceRoller: React.FC = () => {
 
     const baseChance = (21 - difficulty) / 20;
 
-    if (rollType === 'advantage') {
-        const advantageChance = 1 - Math.pow((difficulty - 1) / 20, 2);
-        return (advantageChance * 100).toFixed(2);
-    } 
-    else if (rollType === 'disadvantage') {
-        const disadvantageChance = Math.pow((21 - difficulty) / 20, 2);
-        return (disadvantageChance * 100).toFixed(2);
+    if (rollType === "advantage") {
+      const advantageChance = 1 - Math.pow((difficulty - 1) / 20, 2);
+      return (advantageChance * 100).toFixed(2);
+    } else if (rollType === "disadvantage") {
+      const disadvantageChance = Math.pow((21 - difficulty) / 20, 2);
+      return (disadvantageChance * 100).toFixed(2);
     }
 
     return (baseChance * 100).toFixed(2);
@@ -66,39 +65,8 @@ const DiceRoller: React.FC = () => {
             style={{ width: "35px" }} // Resize the input
           />
         </label>
-        <NumRollsSelector numRolls={numRolls} setNumRolls={setNumRolls}/>
-        <div>
-          <label>
-            <input
-              type="radio"
-              name="rollType"
-              value="advantage"
-              checked={rollType === "advantage"}
-              onChange={() => setRollType("advantage")}
-            />
-            Advantage
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="rollType"
-              value="none"
-              checked={rollType === "none"}
-              onChange={() => setRollType("none")}
-            />
-            Neither
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="rollType"
-              value="disadvantage"
-              checked={rollType === "disadvantage"}
-              onChange={() => setRollType("disadvantage")}
-            />
-            Disadvantage
-          </label>
-        </div>
+        <NumRollsSelector numRolls={numRolls} setNumRolls={setNumRolls} />
+        <RollTypeSelector rollType={rollType} setRollType={setRollType} />
         <div style={{ display: "flex", gap: "10px" }}>
           <button onClick={handleRoll}>Roll Dice</button>
           <button onClick={handleReset}>Reset</button>
